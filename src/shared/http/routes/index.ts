1c770e90645542b8474ca58dca2f22412e8ac39e
NiickSS <nicolas.juner.marcari@gmail.com>
2024-03-16 02:01:02 -0300

    Adicionando middleware de erros

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
+import { AppError } from "@shared/errors/appError";
 import { Router } from "express";
 
 const routes = Router();
 
 routes.get("/", (req, res) => {
+  throw new AppError("Acesso negado");
   res.json({ message: "Opa" });
 });
 
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import cors from "cors";
 import { routes } from "./routes";
+import { AppError } from "@shared/errors/appError";
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,20 @@ app.use(express.json());
 const PORT: number = 3000;
 
 app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+  console.log(error);
+  return res.status(500).json({
+    status: "Error",
+    message: "Erro interno do servidor",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server rodando na porta: ${PORT}`);
