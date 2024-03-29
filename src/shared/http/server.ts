7c298f489609c4284c857570689f87440c3a3f01
NiickSS <nicolas.juner.marcari@gmail.com>
2024-03-29 14:10:31 -0300

    Instalando e configurando swagger

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,15 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
 import cors from "cors";
 import { routes } from "./routes";
 import { AppError } from "@shared/errors/appError";
+import swaggerFile from "../../swagger.json";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 const PORT: number = 3000;
 
