b4c59472ac39ae9d60a0e71879a34ff6a61c8bc3
NiickSS <nicolas.juner.marcari@gmail.com>
2024-04-06 14:26:05 -0300

    Refatorando a entidade Role

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
 import { DataSource } from "typeorm";
 import { CreateRolesTable1712416385102 } from "./migrations/1712416385102-CreateRolesTable";
+import { Role } from "@roles/entities/Role";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1712416385102],
 });
