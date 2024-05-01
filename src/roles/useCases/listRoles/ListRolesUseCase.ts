21e12392eff00b81c566e523d09ee00fea3e12d2
NiickSS <nicolas.juner.marcari@gmail.com>
2024-05-01 13:02:07 -0300

    Refatorando Use Cases

--- src/roles/useCases/listRoles/ListRolesUseCase.ts
+++ src/roles/useCases/listRoles/ListRolesUseCase.ts
@@ -1,10 +1,22 @@
-import { Role } from "@roles/entities/Role";
-import { RolesRpository } from "@roles/repositories/RolesRepository";
+import {
+  RolesPaginateProperties,
+  RolesRpository,
+} from "@roles/repositories/RolesRepository";
+
+type ListRolesUseCaseParams = {
+  page: number;
+  limit: number;
+};
 
 export class ListRolesUseCase {
   constructor(private rolesRepository: RolesRpository) {}
 
-  execute(): Role[] {
-    return this.rolesRepository.findAll();
+  execute({
+    limit,
+    page,
+  }: ListRolesUseCaseParams): Promise<RolesPaginateProperties> {
+    const take = limit;
+    const skip = (Number(page) - 1) * take;
+    return this.rolesRepository.findAll({ page, skip, take });
   }
 }
