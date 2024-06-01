124eccd0718d25776940412c38ab95db415a3b12
NiickSS <nicolas.juner.marcari@gmail.com>
2024-06-01 13:31:22 -0300

    Criando user entitiy

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -3,11 +3,12 @@ import { CreateRolesTable1712416385102 } from "./migrations/1712416385102-Create
 import { Role } from "@roles/entities/Role";
 import { CreateUsersTable1717256434525 } from "./migrations/1717256434525-CreateUsersTable";
 import { AddRoleIDToUsersTable1717258630595 } from "./migrations/1717258630595-AddRoleIDToUsersTable";
+import { User } from "@users/entities/User";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
-  entities: [Role],
+  entities: [Role, User],
   migrations: [
     CreateRolesTable1712416385102,
     CreateUsersTable1717256434525,
