6c646376af8bbfae0225569aa96654dd658691d0
NiickSS <nicolas.juner.marcari@gmail.com>
2024-06-01 13:28:07 -0300

    Criando migração AddRoleIdToUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,10 +2,15 @@ import { DataSource } from "typeorm";
 import { CreateRolesTable1712416385102 } from "./migrations/1712416385102-CreateRolesTable";
 import { Role } from "@roles/entities/Role";
 import { CreateUsersTable1717256434525 } from "./migrations/1717256434525-CreateUsersTable";
+import { AddRoleIDToUsersTable1717258630595 } from "./migrations/1717258630595-AddRoleIDToUsersTable";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
   entities: [Role],
-  migrations: [CreateRolesTable1712416385102, CreateUsersTable1717256434525],
+  migrations: [
+    CreateRolesTable1712416385102,
+    CreateUsersTable1717256434525,
+    AddRoleIDToUsersTable1717258630595,
+  ],
 });
