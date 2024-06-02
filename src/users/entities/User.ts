ad7eaf0eda456f5549e739cb69f3f3ff6101d17d
NiickSS <nicolas.juner.marcari@gmail.com>
2024-06-02 12:34:54 -0300

    Criando relação user-roles

--- src/users/entities/User.ts
+++ src/users/entities/User.ts
@@ -1,4 +1,11 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { Role } from "@roles/entities/Role";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
 @Entity("users")
@@ -17,6 +24,11 @@ export class User {
 
   @Column()
   isAdmin: boolean;
+
+  @ManyToOne(() => Role, {
+    cascade: true,
+  })
+  role: Role;
 
   @Column()
   avatar?: string;
