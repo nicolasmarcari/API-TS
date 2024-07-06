c89600017aeacf42925b1a6ffc7e00614439be8b
NiickSS <nicolas.juner.marcari@gmail.com>
2024-07-05 21:08:35 -0300

    Hotfix: Arrumando query params do ListUsersController

--- src/users/useCases/listUsers/ListUsersController.ts
+++ src/users/useCases/listUsers/ListUsersController.ts
@@ -9,7 +9,7 @@ export class ListUsersController {
     const page =
       req.query.page && Number(req.query.page) > 0 ? Number(req.query.page) : 1;
     const limit =
-      req.query.limit && Number(req.query.page) > 0
+      req.query.limit && Number(req.query.limit) > 0
         ? Number(req.query.limit)
         : 15;
 
