f6e3e1f7b2ded5b99ce5410a2bbc44be4549e7f7
NiickSS <nicolas.juner.marcari@gmail.com>
2024-07-09 14:22:53 -0300

    Criando UpdateAvatarController

---
+++ src/users/useCases/updateAvatar/UpdateAvatarController.ts
@@ -0,0 +1,15 @@
+import { container } from "tsyringe";
+import { UpdateAvatarUseCase } from "./UpdateAvatarUseCase";
+import { Request, Response } from "express";
+import { instanceToInstance } from "class-transformer";
+
+export class UpdateAvatarController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const updateAvatarUseCase = container.resolve(UpdateAvatarUseCase);
+    const user = await updateAvatarUseCase.execute({
+      userId: request.user.id,
+      avatarFileName: request.file.filename,
+    });
+    return response.json(instanceToInstance(user));
+  }
+}
